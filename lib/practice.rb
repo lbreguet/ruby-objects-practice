9344dfa5e28d7a7eeb5278790c91d1f06637d75f
lbreguet <louis-w@live.com>
2017-02-10 17:00:12 -0500

    Practice in progress
    -need to finish it soon

--- lib/practice.rb
+++ lib/practice.rb
@@ -4,7 +4,17 @@ module MBTA
   module Subway
     # All of the subway lines
     class System
+      attr_reader :lines
       def initialize
+        @lines = {
+          'Red' => Line.new('Red', ['South Station', 'Park Street', 'Kendall',
+                        'Central', 'Harvard', 'Porter', 'Davis', 'Alewife']),
+          'Green' => Line.new('Green', ['Government Center', 'Park Street',
+                        'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore']),
+          'Orange' => Line.new('Orange', ['North Station', 'Haymarket',
+                        'Park Street', 'State', 'Downtown Crossing', 'Chinatown',
+                        'Back Bay', 'Forest Hills'])
+        }
       end
 
       def stops_between_stations
